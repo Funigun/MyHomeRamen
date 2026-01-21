ec30c338367f353def94ae65adf35f2bc0090dd3
Patryk <stepnikpa@gmail.com>
2026-01-22 00:38:36 +0100

    update Endpoint builder extensions

--- MyHomeRamen.Api.Common/Endpoint/EndpointBuilderExtensions.cs
+++ MyHomeRamen.Api.Common/Endpoint/EndpointBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using MyHomeRamen.Api.Common.Filter;
 
 namespace MyHomeRamen.Api.Common.Endpoint;
 
@@ -18,6 +19,32 @@ public static class EndpointBuilderExtensions
         return routeHandler;
     }
 
+    public static RouteHandlerBuilder MapStandardValidatedPost<TRequest, TResponse>(this IEndpointRouteBuilder builder, string pattern, Delegate handler)
+    {
+        return builder.MapStandardPost<TRequest, TResponse>(pattern, handler)
+                      .WithValidationFilter<TRequest>();
+    }
+
+    public static RouteHandlerBuilder MapStandardAuthenticatedPost<TRequest, TResponse>(this IEndpointRouteBuilder builder, string pattern, Delegate handler)
+    {
+        return builder.MapStandardPost<TRequest, TResponse>(pattern, handler)
+                      .WithAuthenticationFilter<TRequest>()
+                      .WithValidationFilter<TRequest>();
+    }
+
+    public static RouteHandlerBuilder WithValidationFilter<TRequest>(this RouteHandlerBuilder builder)
+    {
+        return builder.AddEndpointFilter<ValidationFilter<TRequest>>()
+                      .WithMetadata("Uses validation filter");
+    }
+
+    public static RouteHandlerBuilder WithAuthenticationFilter<TRequest>(this RouteHandlerBuilder builder)
+    {
+        return builder.ProducesProblem(StatusCodes.Status403Forbidden)
+                      .AddEndpointFilter<AuthorizationFilter<TRequest>>()
+                      .WithMetadata("Uses authentication filter");
+    }
+
     public static RouteHandlerBuilder MapStandardGet<TResponse>(this IEndpointRouteBuilder builder, string pattern, Delegate handler)
     {
         RouteHandlerBuilder routeHandler = builder.MapGet(pattern, handler)
@@ -27,6 +54,12 @@ public static class EndpointBuilderExtensions
                                                   .WithMetadata(typeof(TResponse).DeclaringType!);
 
         return routeHandler;
+    }
+
+    public static RouteHandlerBuilder MapStandardAuthenticatedGet<TResponse>(this IEndpointRouteBuilder builder, string pattern, Delegate handler)
+    {
+        return builder.MapStandardGet<TResponse>(pattern, handler)
+                      .WithAuthenticationFilter<TResponse>();
     }
 
     public static RouteHandlerBuilder MapStandardPut<TRequest>(this IEndpointRouteBuilder builder, string pattern, Delegate handler)
@@ -41,6 +74,19 @@ public static class EndpointBuilderExtensions
         return routeHandler;
     }
 
+    public static RouteHandlerBuilder MapStandardValidatedPut<TRequest>(this IEndpointRouteBuilder builder, string pattern, Delegate handler)
+    {
+        return builder.MapStandardPut<TRequest>(pattern, handler)
+                      .WithValidationFilter<TRequest>();
+    }
+
+    public static RouteHandlerBuilder MapStandardAuthenticatedPut<TRequest, TDto>(this IEndpointRouteBuilder builder, string pattern, Delegate handler)
+    {
+        return builder.MapStandardPut<TRequest>(pattern, handler)
+                      .WithAuthenticationFilter<TRequest>()
+                      .WithValidationFilter<TDto>();
+    }
+
     public static RouteHandlerBuilder MapStandardDelete<TRequest>(this IEndpointRouteBuilder builder, string pattern, Delegate handler)
     {
         RouteHandlerBuilder routeHandler = builder.MapDelete(pattern, handler)
@@ -50,5 +96,11 @@ public static class EndpointBuilderExtensions
                                                   .WithMetadata(typeof(TRequest).DeclaringType!);
 
         return routeHandler;
+    }
+
+    public static RouteHandlerBuilder MapStandardAuthenticatedDelete<TMarker, TRequest>(this IEndpointRouteBuilder builder, string pattern, Delegate handler)
+    {
+        return builder.MapStandardDelete<TMarker>(pattern, handler)
+                      .WithAuthenticationFilter<TRequest>();
     }
 }
