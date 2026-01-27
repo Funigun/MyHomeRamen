cfe5686ce0ed9959336551a5b30ab1bd80ffcaa0
Patryk <stepnikpa@gmail.com>
2026-01-28 00:56:47 +0100

    init identity domain

---
+++ MyHomeRamen.Identity.Api/Domain/User.cs
@@ -0,0 +1,8 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace MyHomeRamen.Identity.Api.Domain;
+
+public class User : IdentityUser<Guid>
+{
+}
---
+++ MyHomeRamen.Identity.Api/Persistance/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyHomeRamen.Identity.Api.Domain;
+
+namespace MyHomeRamen.Identity.Api.Persistance;
+
+public class AppDbContext : IdentityDbContext<User, Role, Guid>
+{
+    public DbSet<Permission> Permissions { get; set; } = default!;
+
+    public DbSet<RefreshToken> RefreshTokens { get; set; } = default!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.HasDefaultSchema("identity");
+
+        builder.Entity<User>(b =>
+        {
+            b.ToTable("Users");
+
+            b.HasMany<Permission>()
+             .WithMany()
+             .UsingEntity("UserPermissions");
+        });
+
+        builder.Entity<Role>(b =>
+        {
+            b.ToTable("Roles");
+            b.HasMany<Permission>()
+             .WithMany()
+             .UsingEntity("RolePermissions");
+        });
+
+        base.OnModelCreating(builder);
+    }
+}
