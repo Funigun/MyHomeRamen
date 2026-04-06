879ac5aa8ac1b54acdcc5a0fb19d0b6671b156a8
Patryk <stepnikpa@gmail.com>
2026-04-06 15:43:33 +0200

    api + frontend

---
+++ MyHomeRamen.Blazor/MyHomeRamen.Blazor/Features/Menu/Ingredients/Responses/GetIngredientByIdResponse.cs
@@ -0,0 +1,8 @@
+namespace MyHomeRamen.Blazor.Features.Menu.Ingredients.Responses;
+
+public sealed record GetIngredientByIdResponse(
+    Guid Id,
+    string Name,
+    string Description,
+    decimal Price,
+    IEnumerable<Guid> CategoryIds);
