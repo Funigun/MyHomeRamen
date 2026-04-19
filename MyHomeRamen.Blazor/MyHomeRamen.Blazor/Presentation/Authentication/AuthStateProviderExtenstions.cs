56e752bf69c919b286c5cf78ce6bd52958efa00b
Patryk <stepnikpa@gmail.com>
2026-04-20 00:29:54 +0200

    layout refactor, auth state provider extensions

---
+++ MyHomeRamen.Blazor/MyHomeRamen.Blazor/Presentation/Authentication/AuthStateProviderExtenstions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace MyHomeRamen.Blazor.Presentation.Authentication;
+
+public static class AuthStateProviderExtenstions
+{
+    extension(AuthenticationStateProvider authenticationState)
+    {
+        public async Task<string> GetUserName()
+        {
+            return await ((CustomAuthenticationStateProvider)authenticationState).GetCurrentUserNameAsync() ?? string.Empty;
+        }
+
+        public async Task<bool> IsAuthenticated()
+        {
+            return await ((CustomAuthenticationStateProvider)authenticationState).IsAuthenticated();
+        }
+
+        public async Task<bool> IsAdmin()
+        {
+            IEnumerable<string> roles = await ((CustomAuthenticationStateProvider)authenticationState).GetCurrentUserRolesAsync();
+            return roles.Contains("Admin");
+        }
+
+        public async Task<bool> IsEmployee()
+        {
+            IEnumerable<string> roles = await ((CustomAuthenticationStateProvider)authenticationState).GetCurrentUserRolesAsync();
+            return roles.Contains("Employee");
+        }
+
+        public async Task<bool> IsCustomer()
+        {
+            IEnumerable<string> roles = await ((CustomAuthenticationStateProvider)authenticationState).GetCurrentUserRolesAsync();
+            return roles.Contains("Customer");
+        }
+    }
+}
