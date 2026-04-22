b05b29718c9635fd357b94725d50c0bd5473c53d
Patryk <stepnikpa@gmail.com>
2026-04-22 22:36:28 +0200

    get_user_details_endpoint

---
+++ MyHomeRamen.Identity.Api/Features/Account/GetDetails/Models/GetDetailsRequest.cs
@@ -0,0 +1,5 @@
+using MyHomeRamen.Api.Common.Endpoint.Models;
+
+namespace MyHomeRamen.Identity.Api.Features.Account.GetDetails.Models;
+
+public sealed record GetDetailsRequest : IRequest<GetDetailsResponse>;
---
+++ MyHomeRamen.Identity.Api/Features/Account/GetDetails/Models/GetDetailsResponse.cs
@@ -0,0 +1,3 @@
+namespace MyHomeRamen.Identity.Api.Features.Account.GetDetails.Models;
+
+public sealed record GetDetailsResponse(string Username, string FirstName, string LastName, string Email, string PhoneNumber);
