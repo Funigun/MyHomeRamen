1d749b517d58caa63da40560e38acf47d085ad87
Patryk <33098972+Funigun@users.noreply.github.com>
2026-04-22 22:12:23 +0200

    feat: get adresses (#55)

---
+++ MyHomeRamen.Identity.Api/Features/Account/Addresses/GetAddresses/Models/GetAddressesRequest.cs
@@ -0,0 +1,5 @@
+using MyHomeRamen.Api.Common.Endpoint.Models;
+
+namespace MyHomeRamen.Identity.Api.Features.Account.Addresses.GetAddresses.Models;
+
+public sealed record GetAddressesRequest : IRequest<GetAddressesResponse>;
